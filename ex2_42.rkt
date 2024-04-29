5076f4b250d1134324891d72da0f9dd81026d00b
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-04-29 10:07:33 +0100

    2.42 completed - written from scratch, not following book suggestion

--- ex2_42.rkt
+++ ex2_42.rkt
@@ -1,12 +1,76 @@
 #lang racket
 
+(require "flatmap.rkt")
+
 ;; Return a list of ways to arrange n queens on an nxn board with no queens attacking each other
 #|
 Strategy:
   - Represent a board as a list of pairs, telling us the coordinates of all the queens placed on it
   - Pairs representing queen coords (x y): x in [1,n] tells us the column y in [1,n] tells us the row
+  - Generate a list of all boards that are valid answers. Start by enumerating all possible positions with the queen in the first column. Then for each of those positions, enumerate all positions with a queen in the next column and filter out the invalid positions where the new queen is attacking an existing queen.
 |#
-(define (queens n)
-  (error "not done yet" n))
+
+(define (make-coord x y)
+  (list x y))
+
+(define (coord-col c)
+  (car c))
+
+(define (coord-row c)
+  (cadr c))
+
+(define (new-position-valid? position board)
+  (not
+   (or
+    (column-collision? position board)
+    (row-collision? position board)
+    (diagonal-collision? position board))))
+
+(define (column-collision? position board)
+  (cond
+    [(null? board) #f]
+    [(= (coord-col position) (coord-col (car board))) #t]
+    [else (column-collision? position (cdr board))]))
+
+(define (row-collision? position board)
+  (cond
+    [(null? board) #f]
+    [(= (coord-row position) (coord-row (car board))) #t]
+    [else (row-collision? position (cdr board))]))
+
+(define (diagonal-collision? position board)
+  (cond
+    [(null? board) #f]
+    [(= (- (coord-row position) (coord-row (car board))) (- (coord-col position) (coord-col (car board)))) #t]
+    [(= (- (coord-row position) (coord-row (car board))) (- (coord-col (car board)) (coord-col position))) #t]
+    [else (diagonal-collision? position (cdr board))]))
+
+(define (enumerate-column col board-size)
+  (define (iter i out)
+    (if (> i board-size)
+        out
+        (iter (+ i 1) (cons (make-coord col i) out))))
+  (iter 1 '()))
+
+(define (queens board-size)
+  (define (make-valid-boards new-coord old-boards)
+    (define (iter boards-in boards-out)
+      (cond [(null? boards-in) boards-out]
+            [(new-position-valid? new-coord (car boards-in))
+             (iter (cdr boards-in) (cons (cons new-coord (car boards-in)) boards-out))]
+            [else
+             (iter (cdr boards-in) boards-out)]))
+    (iter old-boards '()))
+
+  (define (update-boards-with-column col boards)
+    (flatmap (lambda (new-coord)
+               (make-valid-boards new-coord boards))
+             (enumerate-column col board-size)))
+
+  (define (queens-iter col boards)
+    (if (> col board-size)
+        boards
+        (queens-iter (+ 1 col) (update-boards-with-column col boards))))
+  (queens-iter 2 (map (lambda (coord) (list coord)) (enumerate-column 1 board-size))))
 
 (provide queens)
--- ex2_42.test.rkt
+++ ex2_42.test.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (require rackunit
-		 "ex2_42.rkt")
+         "ex2_42.rkt")
 
 (check-equal? (length (queens 1)) 1)
 (check-equal? (length (queens 2)) 0)
@@ -16,3 +16,13 @@
 (check-equal? (length (queens 11)) 2680)
 (check-equal? (length (queens 12)) 14200)
 
+;; taking advantage of an implementation detail, my solutions always generate in descending order by column so we can check an example without handling the 
+(define (contains? elem lst)
+  (cond [(null? lst) #f]
+		[(equal? elem (car lst)) #t]
+		[else (contains? elem (cdr lst))]))
+
+(define book-example-solution
+  '((8 3) (7 5) (6 8) (5 4) (4 1) (3 7) (2 2) (1 6)))
+
+(check-true (contains? book-example-solution (queens 8)))
